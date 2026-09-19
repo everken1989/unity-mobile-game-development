497b53162772bf541f369456dbc83248feebb34a
everolivas <everolivas1989@hotmail.com>
2026-09-19 10:56:43 -0600

    Clase 3
    Agregar Materiales
    Cambios en Scripts

--- Assets/Scripts/fundamentos.cs
+++ Assets/Scripts/fundamentos.cs
@@ -10,24 +10,29 @@ public class fundamentos : MonoBehaviour
     //FPS: ¿Como podemos manipular el FPS?
     //int number = 21; 
     //string miNombre = "Su Karla";
-    public float speedX;
-    public float speedY;
-    public float speedZ;
-    //public bool isActive = true;
+    public float speed;
+    public float angulo;
+    public float moveX, moveZ;
+
     void Start()
     {
-        Application.targetFrameRate = 60;
-        print("Mi FPS: " + Application.targetFrameRate);
-        float fps = 1f / Time.deltaTime;
-        print("Real FPS: " + fps);
+        
     }
     //Que se ejecute 
     void Update()
     {
-        
-        float rTime = Time.deltaTime; 
+        float rTime = Time.deltaTime;
+        moveX = Input.GetAxis("Horizontal");
+        //Declarar axis en el eje que sigue 
+        //Incorporar en el Translate 
         //Coords X Y Z 
-        transform.Translate(speedX, speedY * rTime, speedZ * rTime);
+        //transform.Translate(moveX * speed * rTime, 0, moveZ * speed * rTime);
+        //Acumulador
+        transform.localScale += new Vector3(1f, 1f, 1f);
+        //Posicion al Translate - Escala - Rotation
+                          // X Y Z 
+        transform.Rotate(angulo,0f,0f);  
+        //Escala ++
         
     }
 }
